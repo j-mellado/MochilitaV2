0369b1833016259fe3df5c5e8bd78cf63e92897e
Jesus Mellado Ramos <jmellado@c1r7s1.42malaga.com>
2024-09-12 12:01:01 +0200

    lib y primeras funciones

---
+++ libft.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmellado <jmellado@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/11 11:38:14 by jmellado          #+#    #+#             */
+/*   Updated: 2024/09/11 16:45:36 by jmellado         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+#include <string.h>
+
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+size_t	ft_strlen(const char *s);
+#endif
