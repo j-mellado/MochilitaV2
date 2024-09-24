5d6baa0d4d8864f59879a01f41f84316aa15369d
Jesus <melladoramosjesus@gmail.com>
2024-09-25 01:15:20 +0200

    ft strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -14,14 +14,23 @@
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
-	size_t	i;
+	size_t srclen;
+	size_t i;
 
 	i = 0;
-	while (src[i] != '\0')
+	srclen = ft_strlen(src);
+
+	if (size == 0)
 	{
-		dst[i] = src[i]; // por hacer todo aun
+		return (srclen);
+	}
+
+	while (i < (size - 1) && src[i] != '\0')
+	{
+		dst[i] = src[i];
 		i++;
 	}
-	dst[i] = '\0';
-	return (dst);
+		dst[i] = '\0';
+
+	return (srclen);
 }
