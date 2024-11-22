1761de974efed4238050263b25e7f9e64bfa3dbd
Jesus Mellado Ramos <jmellado@c3r8s3.42malaga.com>
2024-11-22 14:53:14 +0100

    pasa paco

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,27 +6,28 @@
 /*   By: jmellado <jmellado@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 14:32:14 by jmellado          #+#    #+#             */
-/*   Updated: 2024/11/22 14:51:49 by jmellado         ###   ########.fr       */
+/*   Updated: 2024/11/22 14:52:29 by jmellado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-    t_list *new_list = NULL;
-    t_list *new_node;
+	t_list	*new_list;
+	t_list	*new_node;
 
-    while (lst != NULL)
-    {
-        new_node = ft_lstnew(f(lst->content));
-        if (new_node == NULL)
-        {
-            ft_lstclear(&new_list, del);
-            return NULL;
-        }
-        ft_lstadd_back(&new_list, new_node);
-        lst = lst->next;
-    }
-    return new_list;
+	new_list = NULL;
+	while (lst != NULL)
+	{
+		new_node = ft_lstnew(f(lst->content));
+		if (new_node == NULL)
+		{
+			ft_lstclear(&new_list, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&new_list, new_node);
+		lst = lst->next;
+	}
+	return (new_list);
 }
